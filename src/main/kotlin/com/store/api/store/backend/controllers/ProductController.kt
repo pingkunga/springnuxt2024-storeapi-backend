96e527ba888aba28444410b8cf2c74a507a2afdb
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-07-17 21:42:42 +0700

    Add Swagger Doc for Product

--- src/main/kotlin/com/store/api/store/backend/controllers/ProductController.kt
+++ src/main/kotlin/com/store/api/store/backend/controllers/ProductController.kt
@@ -2,18 +2,23 @@ package com.store.api.store.backend.controllers
 
 import com.store.api.store.backend.models.Product
 import com.store.api.store.backend.services.ProductService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Products", description = "APIs for managing products")
 @RestController
 @RequestMapping("/api/v1/products")
 class ProductController(private val productService: ProductService) {
     // Get all products
+    @Operation(summary = "Get all products", description = "Get all products from database")
     @GetMapping
     fun getAllProducts() = productService.getAllProducts()
 
     // Get product by id
+    @Operation(summary = "Get product by ID", description = "Get product by ID from database")
     @GetMapping("/{id}")
     fun getProductById(@PathVariable id: Int): ResponseEntity<Product> {
         val  product = productService.getProductById(id)
@@ -22,6 +27,7 @@ class ProductController(private val productService: ProductService) {
     }
 
     // Create product
+    @Operation(summary = "Create new product", description = "Create new product in database")
     @PostMapping
     fun createProduct(@RequestBody product: Product): ResponseEntity<Product> {
         val createdProduct = productService.createProduct(product)
@@ -29,6 +35,7 @@ class ProductController(private val productService: ProductService) {
     }
 
     // Update Product
+    @Operation(summary = "Update product by ID", description = "Update product by ID from database")
     @PutMapping("/{id}")
     fun updateProduct(@PathVariable id: Int, @RequestBody product: Product): ResponseEntity<Product> {
         val updatedProduct = productService.updateProduct(id, product)
@@ -36,6 +43,7 @@ class ProductController(private val productService: ProductService) {
     }
 
     // Delete Product
+    @Operation(summary = "Delete product by ID", description = "Delete product by ID from database")
     @DeleteMapping("/{id}")
     fun deleteProduct(@PathVariable id: Int): ResponseEntity<Void> {
         productService.deleteProduct(id)

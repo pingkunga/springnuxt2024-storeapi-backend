a0ec4462055f7d0edb2cf4d9bc496db1d1c63836
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-07-17 21:42:35 +0700

    Add Swagger Doc for Category

--- src/main/kotlin/com/store/api/store/backend/controllers/CategoryController.kt
+++ src/main/kotlin/com/store/api/store/backend/controllers/CategoryController.kt
@@ -2,14 +2,18 @@ package com.store.api.store.backend.controllers
 
 import com.store.api.store.backend.models.Category
 import com.store.api.store.backend.services.CategoryService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "Categories", description = "APIs for managing categories")
 @RestController
 @RequestMapping("/api/v1/categories")
 class CategoryController(private val categoryService: CategoryService) {
 
     // GET /api/categories
+    @Operation(summary = "Get all categories", description = "Get all categories from database")
     @GetMapping
     fun getCategories(): ResponseEntity<List<Category>> {
         val categories = categoryService.getAllCategories()
@@ -17,6 +21,7 @@ class CategoryController(private val categoryService: CategoryService) {
     }
 
     // GET /api/categories/{id}
+    @Operation(summary = "Get category by ID", description = "Get category by ID from database")
     @GetMapping("/{id}")
     fun getCategory(@PathVariable id: Int): ResponseEntity<Category> {
         val category = categoryService.getCategoryById(id)
@@ -28,6 +33,7 @@ class CategoryController(private val categoryService: CategoryService) {
     }
 
     // POST /api/categories
+    @Operation(summary = "Add new category", description = "Add new category to database")
     @PostMapping
     fun addCategory(@RequestBody category: Category): ResponseEntity<Category> {
         val savedCategory = categoryService.addCategory(category)
@@ -35,6 +41,7 @@ class CategoryController(private val categoryService: CategoryService) {
     }
 
     // PUT /api/categories/{id}
+    @Operation(summary = "Update category by ID", description = "Update category by ID from database")
     @PutMapping("/{id}")
     fun updateCategory(@PathVariable id: Int, @RequestBody category: Category): ResponseEntity<Category> {
         val updatedCategory = categoryService.updateCategory(id, category)
@@ -46,6 +53,7 @@ class CategoryController(private val categoryService: CategoryService) {
     }
 
     // DELETE /api/categories/{id}
+    @Operation(summary = "Delete category by ID", description = "Delete category by ID from database")
     @DeleteMapping("/{id}")
     fun deleteCategory(@PathVariable id: Int): ResponseEntity<Category> {
         val deletedCategory = categoryService.deleteCategory(id)

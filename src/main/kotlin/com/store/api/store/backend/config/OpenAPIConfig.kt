141c72e543a837b50e4852069254b437f0b47119
Chatri Ngambenchawong <pingkunga@gmail.com>
2024-07-18 21:02:08 +0700

    Add Swagger Doc

--- src/main/kotlin/com/store/api/store/backend/config/OpenAPIConfig.kt
+++ src/main/kotlin/com/store/api/store/backend/config/OpenAPIConfig.kt
@@ -1,12 +1,18 @@
 package com.store.api.store.backend.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class OpenAPIConfig {
+
+    // JWT
+    val securitySchemeName = "bearerAuth"
 
     @Bean
     fun customOpenAPI(): OpenAPI {
@@ -16,6 +22,19 @@ class OpenAPIConfig {
                     .title("Store API with Spring Boot and PostgreSQL")
                     .version("1.0")
                     .description("This is Store API using Spring Boot and PostgreSQL")
+            )
+            // Add Security JWT
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        securitySchemeName,
+                        SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
             )
             .tags(
                 listOf(
